4e816f7acbfe6201535cc6168b812ec078b8f22e
Daniel Medina <danielme_com@yahoo.com>
2018-10-07 12:20:54 +0200

    use blank spaces instead of tabs; unusued import;

--- src/main/java/com/danielme/springboot/controllers/CountryController.java
+++ src/main/java/com/danielme/springboot/controllers/CountryController.java
@@ -1,7 +1,6 @@
 package com.danielme.springboot.controllers;
 
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,19 +12,20 @@ import com.danielme.springboot.repositories.CountryRepository;
 public class CountryController {
 
     private static final Logger logger = Logger.getLogger(CountryController.class);
-    
-	private final CountryRepository countryRepository;
-	private final CustomProperties customProperties;
-	
-	public CountryController(CountryRepository countryRepository, CustomProperties customProperties) {
-	    this.countryRepository = countryRepository;
-	    this.customProperties = customProperties;
-	}
 
-	@RequestMapping("/")
-	public String list(Model model) {
-	    logger.info(customProperties.getValue());
-		model.addAttribute("countriesList", countryRepository.findAll());
-		return "countriesList";
-	}
+    private final CountryRepository countryRepository;
+    private final CustomProperties customProperties;
+
+    public CountryController(CountryRepository countryRepository,
+            CustomProperties customProperties) {
+        this.countryRepository = countryRepository;
+        this.customProperties = customProperties;
+    }
+
+    @RequestMapping("/")
+    public String list(Model model) {
+        logger.info(customProperties.getValue());
+        model.addAttribute("countriesList", countryRepository.findAll());
+        return "countriesList";
+    }
 }
