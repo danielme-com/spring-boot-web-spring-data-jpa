6122c68c78793a675df043c36fdbd9ccfb9a1a21
Daniel Medina <danielme_com@yahoo.com>
2019-02-28 20:18:51 +0100

    use bean validation

--- src/main/java/com/danielme/springboot/CustomProperties.java
+++ src/main/java/com/danielme/springboot/CustomProperties.java
@@ -1,10 +1,15 @@
 package com.danielme.springboot;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import javax.validation.constraints.NotBlank;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
 @ConfigurationProperties("custom")
 public class CustomProperties {
 
+    @NotBlank
     private String value;
 
     public String getValue() {
