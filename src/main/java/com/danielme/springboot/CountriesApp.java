a63a79d6b9c0e8df8200a5311ff7c46d50611ebe
dani <danielme_com@yahoo.com>
2022-07-26 14:43:49 +0200

    se elimina las custom properties, se explican en un tutorial independiente

--- src/main/java/com/danielme/springboot/CountriesApp.java
+++ src/main/java/com/danielme/springboot/CountriesApp.java
@@ -1,36 +1,30 @@
 package com.danielme.springboot;
 
+import com.danielme.springboot.services.CountryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import com.danielme.springboot.services.CountryService;
 
 @SpringBootApplication
-//@EnableConfigurationProperties(CustomProperties.class)
-@ConfigurationPropertiesScan
-public class CountriesApp extends SpringBootServletInitializer// implements CommandLineRunner
-{
+public class CountriesApp extends SpringBootServletInitializer /*implements CommandLineRunner*/ {
 
     //private static final Logger logger = LoggerFactory.getLogger(CountriesApp.class);
 
-    // @Autowired
-    // CountryService countryService;
+    /*@Autowired
+    CountryService countryService;*/
 
     public static void main(String[] args) {
         SpringApplication.run(CountriesApp.class, args);
     }
 
-    /*
-     * @Override public void run(String... arg0) throws Exception {
-     * logger.info(countryService.findAll().size()); }
-     */
+    /*@Override
+    public void run(String... arg0) {
+        int size = countryService.findAll().size();
+        logger.info("countries: {}", size);
+    }*/
 
 }
--- src/main/java/com/danielme/springboot/controllers/CountryController.java
+++ src/main/java/com/danielme/springboot/controllers/CountryController.java
@@ -1,31 +1,21 @@
 package com.danielme.springboot.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.danielme.springboot.services.CountryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.danielme.springboot.CustomProperties;
-import com.danielme.springboot.services.CountryService;
-
 @Controller
 public class CountryController {
 
-    private static final Logger logger = LoggerFactory.getLogger(CountryController.class);
-
     private final CountryService countryService;
-    private final CustomProperties customProperties;
 
-    public CountryController(CountryService countryService,
-            CustomProperties customProperties) {
+    public CountryController(CountryService countryService) {
         this.countryService = countryService;
-        this.customProperties = customProperties;
     }
 
     @RequestMapping("/")
     public String list(Model model) {
-        logger.info(customProperties.getValue());
         model.addAttribute("countriesList", countryService.findAll());
         return "countriesList";
     }
