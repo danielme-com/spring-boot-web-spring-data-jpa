17f5cf6a65f400ff3317d7771fc5239fdafe346a
dani <danielme_com@yahoo.com>
2023-03-04 12:24:36 +0100

    convertir CountryService en interfaz

--- src/main/java/com/danielme/springboot/services/CountryService.java
+++ src/main/java/com/danielme/springboot/services/CountryService.java
@@ -2,54 +2,24 @@ package com.danielme.springboot.services;
 
 import com.danielme.springboot.entities.Country;
 import com.danielme.springboot.model.CountryRequest;
-import com.danielme.springboot.repositories.CountryRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class CountryService {
+public interface CountryService {
 
-    private final CountryRepository countryRepository;
+    List<Country> findAll();
 
-    public CountryService(CountryRepository countryRepository) {
-        this.countryRepository = countryRepository;
-    }
+    Optional<Country> findById(Long id);
 
-    public List<Country> findAll() {
-        return countryRepository.findAll();
-    }
+    Long create(CountryRequest newCountry);
 
-    public Optional<Country> findById(Long id) {
-        return countryRepository.findById(id);
-    }
+    boolean update(Long id, CountryRequest countryRequest);
 
-    public Long create(CountryRequest newCountry) {
-        Country countryEntity = new Country(newCountry.getName(), newCountry.getPopulation());
-        return countryRepository.save(countryEntity).getId();
-    }
-
-    @Transactional
-    public boolean update(Long id, CountryRequest countryRequest) {
-        return countryRepository.findById(id)
-                .map(country -> {
-                    copy(countryRequest, country);
-                    return true;
-                })
-                .orElse(false);
-    }
-
-    private void copy(CountryRequest countryRequest, Country country) {
+    default void copy(CountryRequest countryRequest, Country country) {
         country.setName(countryRequest.getName());
         country.setPopulation(countryRequest.getPopulation());
     }
 
-    public void delete(Long id) {
-        if (countryRepository.existsById(id)) {
-            countryRepository.deleteById(id);
-        }
-    }
-
+    void delete(Long id);
 }
---
+++ src/main/java/com/danielme/springboot/services/CountryServiceImpl.java
@@ -0,0 +1,56 @@
+package com.danielme.springboot.services;
+
+import com.danielme.springboot.entities.Country;
+import com.danielme.springboot.model.CountryRequest;
+import com.danielme.springboot.repositories.CountryRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CountryServiceImpl implements CountryService {
+
+
+    private final CountryRepository countryRepository;
+
+    public CountryServiceImpl(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @Override
+    public List<Country> findAll() {
+        return countryRepository.findAll();
+    }
+
+    @Override
+    public Optional<Country> findById(Long id) {
+        return countryRepository.findById(id);
+    }
+
+    @Override
+    public Long create(CountryRequest newCountry) {
+        Country countryEntity = new Country(newCountry.getName(), newCountry.getPopulation());
+        return countryRepository.save(countryEntity).getId();
+    }
+
+    @Override
+    @Transactional
+    public boolean update(Long id, CountryRequest countryRequest) {
+        return countryRepository.findById(id)
+                .map(country -> {
+                    copy(countryRequest, country);
+                    return true;
+                })
+                .orElse(false);
+    }
+
+    @Override
+    public void delete(Long id) {
+        if (countryRepository.existsById(id)) {
+            countryRepository.deleteById(id);
+        }
+    }
+
+}
